bc68e4110ca43a1e7e8fa980504666d4caedd39c
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-10-15 12:40:39 -0400

    modifier slogan page accueil

--- creative/resources/views/home.blade.php
+++ creative/resources/views/home.blade.php
@@ -10,7 +10,7 @@
 				<hr class="divider" />
 			</div>
 			<div class="col-lg-8 align-self-baseline">
-				<p class="text-white-75 mb-5">Immortilize our special day with quality pictures</p>
+				<p class="text-white-75 mb-5">Immortilize your special day!</p>
 				<a class="btn btn-primary btn-xl" href="#about">Find Out More</a>
 			</div>
 		</div>
